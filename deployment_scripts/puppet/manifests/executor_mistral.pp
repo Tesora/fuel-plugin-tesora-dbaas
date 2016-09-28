c07326d69e827200ed85a11a71ce54f1880e6216
Brian Hunter <bhunter@tesora.com>
2016-09-28 18:21:48 -0400

    skeleton for mistral service install orchestration
    Change-Id: I6c965eb607e616ede86f9519d57b8ff64abd7dbf

---
+++ deployment_scripts/puppet/manifests/executor_mistral.pp
@@ -0,0 +1,12 @@
+#
+# Copyright (c) 2016 Tesora Inc.  All Rights Reserved.
+#
+# All use, reproduction, transfer, publication or disclosure is prohibited
+# except as may be expressly permitted in the applicable license agreement.
+#
+
+notice('MODULAR: tesora_mistral/executor_mistral.pp')
+
+include tesora_mistral
+
+class {'tesora_mistral::executor':}
