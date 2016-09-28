83acd3f11e3c703a2bdf6e3131bb9fe6ef92ba0a
Brian Hunter <bhunter@tesora.com>
2016-09-28 18:21:48 -0400

    skeleton for mistral service install orchestration
    Change-Id: I6c965eb607e616ede86f9519d57b8ff64abd7dbf

---
+++ deployment_scripts/puppet/modules/tesora_mistral/manifests/dashboard_mistral.pp
@@ -0,0 +1,34 @@
+#
+# Copyright (c) 2016 Tesora Inc.  All Rights Reserved.
+#
+# All use, reproduction, transfer, publication or disclosure is prohibited
+# except as may be expressly permitted in the applicable license agreement.
+#
+
+class tesora_mistral::dashboard_mistral {
+
+  include ::horizon::params
+
+  package { 'python-pip':
+    ensure => installed,
+  } ->
+  package { 'python-dev':
+    ensure => installed,
+  } ->
+  package { 'mistral-dashboard':
+    ensure   => $tesora_mistral::dashboard_version,
+    name     => $tesora_mistral::dashboard_name,
+    provider => pip,
+  } ->
+  file { $tesora_mistral::horizon_ext_file:
+    ensure  => file,
+    content => template('tesora_mistral/_50_mistral.py.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  } ~>
+  service { $horizon::params::http_service:
+    ensure => running,
+  }
+
+}
---
+++ deployment_scripts/puppet/modules/tesora_mistral/manifests/db_sync_mistral.pp
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2016 Tesora Inc.  All Rights Reserved.
+#
+# All use, reproduction, transfer, publication or disclosure is prohibited
+# except as may be expressly permitted in the applicable license agreement.
+#
+
+class tesora_mistral::db_sync {
+
+  Exec <| title == 'mistral-db-sync' |> {
+    refreshonly => false,
+  }
+
+  Exec <| title == 'mistral-db-populate' |> {
+    refreshonly => false,
+  }
+
+  include mistral::db::sync
+
+}
