aa4a55ad53b5499f270882348ec964ff567aaa8d
iduodu <iduodu@tesora.com>
2016-10-05 16:15:58 -0400

    Update setup.sh script
    JIRA: DBAAS-2263
    1. Cinder, Swift, and Nova are no longer needed
    2. Update evrything else that needs to be updated
    
    Change-Id: Icddc3fb36a61ce1f99699e430e75b2543117f0b3

--- deployment_scripts/puppet/modules/tesora_dbaas/files/setup.sh
+++ deployment_scripts/puppet/modules/tesora_dbaas/files/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 #
-# Copyright (c) 2014 Tesora Inc.  All Rights Reserved.
+# Copyright (c) 2016 Tesora Inc.  All Rights Reserved.
 #
 # All use, reproduction, transfer, publication or disclosure is prohibited
 # except as may be expressly permitted in the applicable license agreement.
@@ -63,16 +63,9 @@ $option = $value
 }
 
 # Shut down Tesora DBaaS to avoid resource errors
-if [ -f "/etc/debian_version" ]
-then
-    service trove-api stop
-    service trove-taskmanager stop
-    service trove-conductor stop
-else
-    service openstack-trove-api stop
-    service openstack-trove-taskmanager stop
-    service openstack-trove-conductor stop
-fi
+service trove-api stop
+service trove-taskmanager stop
+service trove-conductor stop
 
 db_hostport=${opt_mysqL_hostport}
 
@@ -84,7 +77,8 @@ db_pass=${opt_trove_mysql_pass}
 db_admin_user=${opt_mysql_admin_user}
 db_admin_password=${opt_mysql_admin_pass}
 
-sql_connection="mysql://$db_user:$db_pass@$db_hostport/trove\?charset=utf8"
+database="trove"
+sql_connection="mysql+pymysql://${db_user}:${db_pass}@${db_host}:${db_port}/${database}\?charset=utf8"
 
 trove_user=${opt_trove_admin_user}
 trove_password=${opt_trove_admin_pass}
@@ -93,27 +87,22 @@ trove_region=${opt_trove_region}
 
 keystone_admin_url=${opt_keystone_admin_url}
 keystone_public_url=${opt_keystone_public_url}
-echo "BRIAN ${opt_keystone_public_url} ${keystone_public_url}"
-keystone_admin_port=$( echo $keystone_admin_url | sed -e "s/.*:\/\/.*:\([\0-9]\+\).*/\1/" )
 keystone_admin_user=${opt_keystone_admin_user}
 keystone_admin_password=${opt_keystone_admin_pass}
 keystone_admin_tenant=${opt_keystone_admin_tenant}
+keystone_host=$( echo $keystone_admin_url | sed -e "s/.*:\/\/\(.*\):[\0-9]\+.*/\1/" )
+keystone_admin_port=$( echo $keystone_admin_url | sed -e "s/.*:\/\/.*:\([\0-9]\+\).*/\1/" )
+keystone_unversioned_admin_url=$(echo $keystone_admin_url | sed 's/\(http[s]*:\/\/[0-9\.]*:[0-9]*\).*/\1/' )
+keystone_unversioned_public_url=${keystone_unversioned_admin_url}
+guest_keystone_host=${keystone_host}
 
-nova_url=${opt_nova_url}
-nova_user=${opt_nova_user}
-nova_password=${opt_nova_pass}
-nova_tenant=${opt_nova_tenant}
-
-cinder_url=${opt_cinder_url}
-
-swift_admin_url="${opt_swift_admin_url}/AUTH_"
-swift_public_url="${opt_swift_public_url}/AUTH_"
-
-rabbit_hosts=${opt_rabbit_hosts}
+rabbit_host=${opt_rabbit_hosts}
+rabbit_port=${opt_rabbit_port}
 rabbit_userid=${opt_rabbit_user}
 rabbit_password=${opt_rabbit_pass}
 
-guest_rabbit_hosts="$opt_guest_rabbit_hosts"
+guest_rabbit_host="$opt_guest_rabbit_hosts"
+guest_keystone_host=${keystone_host}
 
 trove_user=${opt_trove_admin_user}
 trove_password=${opt_trove_admin_pass}
@@ -127,54 +116,52 @@ trove_admin_endpoint="${trove_admin_url}/v1.0/\$(tenant_id)s"
 
 
 # Tailor configuration files
+for file in /etc/trove/trove.conf /etc/trove/trove-conductor.conf /etc/trove/trove-guestagent.conf /etc/trove/trove-taskmanager.conf; do
+    ini_set "$file" DEFAULT rpc_backend rabbit
 
-for file in /etc/trove/trove.conf /etc/trove/trove-taskmanager.conf /etc/trove/trove-conductor.conf /etc/trove/trove-guestagent.conf; do
-    # set messaging credentials
-    ini_set "$file" oslo_messaging_rabbit rabbit_hosts "$rabbit_hosts"
-    ini_set "$file" oslo_messaging_rabbit rabbit_userid $rabbit_userid
-    ini_set "$file" oslo_messaging_rabbit rabbit_password $rabbit_password
-
-    # set OpenStack credentials
-    ini_set "$file" DEFAULT nova_proxy_admin_user $nova_user
-    ini_set "$file" DEFAULT nova_proxy_admin_pass $nova_password
-    ini_set "$file" DEFAULT trove_auth_url $keystone_admin_url
-
-    ini_set "$file" DEFAULT swift_url $swift_admin_url
+    ini_set "$file" oslo_messaging_rabbit rabbit_host ${rabbit_host}
+    ini_set "$file" oslo_messaging_rabbit rabbit_port ${rabbit_port}
+    ini_set "$file" oslo_messaging_rabbit rabbit_userid ${rabbit_userid}
+    ini_set "$file" oslo_messaging_rabbit rabbit_password ${rabbit_password}
 
     [ -n "${trove_region}" ] && ini_set "$file" DEFAULT os_region_name $trove_region
-
-    # set these for everything EXCEPT the guest agent
-    if [[ ! $file =~ "trove-guestagent" ]]
-    then
-        ini_set "$file" database connection $sql_connection
-        ini_set "$file" DEFAULT nova_compute_url $nova_url
-        ini_set "$file" DEFAULT cinder_url $cinder_url
-
-        [ -n "${opt_use_nova_key_name}" ] && ini_set "$file" DEFAULT use_nova_key_name $opt_use_nova_key_name
-        [ -n "${opt_use_nova_server_config_drive}" ] && ini_set "$file" DEFAULT use_nova_server_config_drive $opt_use_nova_server_config_drive
-        [ -n "${opt_network_driver}" ] && ini_set "$file" DEFAULT network_driver $opt_network_driver
-        [ -n "${opt_cinder_service_type}" ] && ini_set "$file" DEFAULT cinder_service_type $opt_cinder_service_type
-        [ -n "${opt_network_label_regex}" ] && ini_set "$file" DEFAULT network_label_regex $opt_network_label_regex
-    fi
 done
 
-file=/etc/trove/trove-guestagent.conf
-ini_set "$file" DEFAULT trove_auth_url "$keystone_public_url"
-ini_set "$file" DEFAULT swift_url "$swift_public_url"
-ini_set "$file" oslo_messaging_rabbit rabbit_hosts "$guest_rabbit_hosts"
+for file in /etc/trove/trove.conf /etc/trove/trove-conductor.conf /etc/trove/trove-taskmanager.conf; do
+   sql_connection="mysql+pymysql://${db_user}:${db_pass}@${db_host}:${db_port}/trove\?charset=utf8"
+   ini_set "$file" database connection $sql_connection
 
-file=/etc/trove/trove.conf
-ini_set "$file" DEFAULT bind_host $opt_trove_bind_host
+   ini_set "$file" DEFAULT trove_auth_url $keystone_public_url
+done
 
-ini_set "$file" keystone_authtoken auth_host $opt_controller_host
-ini_set "$file" keystone_authtoken auth_port $keystone_admin_port
-ini_set "$file" keystone_authtoken auth_protocol http
-ini_set "$file" keystone_authtoken admin_tenant_name $trove_tenant
-ini_set "$file" keystone_authtoken admin_user $trove_user
-ini_set "$file" keystone_authtoken admin_password $trove_password
-ini_set "$file" keystone_authtoken auth_version v2.0
-[ -n "${opt_auth_protocol}" ] && ini_set "$file" keystone_authtoken auth_protocol $opt_auth_protocol
+for file in /etc/trove/trove.conf /etc/trove/trove-taskmanager.conf; do
+    if [ -n "${opt_network_driver}" ]; then
+        ini_set "$file" DEFAULT network_driver ${opt_network_driver}
+    elif [[ -n "${neutron_public_url}" ]]; then
+        ini_set "$file" DEFAULT network_driver trove.network.neutron.NeutronDriver
+    else
+        ini_set "$file" DEFAULT network_driver trove.network.nova.NovaNetwork
+    fi
 
+    [ -n "${opt_network_label_regex}" ] && ini_set "$file" DEFAULT network_label_regex $opt_network_label_regex
+done
+
+
+if [ "$guest_keystone_host" != "$keystone_host" ]; then
+    file=/etc/trove/trove-guestagent.conf
+    ini_set "$file" DEFAULT trove_auth_url "${keystone_public_url/$keystone_host/$guest_keystone_host}"
+    ini_set "$file" oslo_messaging_rabbit rabbit_host "$guest_keystone_host"
+fi
+
+for file in /etc/trove/trove.conf; do
+    ini_set "$file" keystone_authtoken auth_uri $keystone_unversioned_public_url
+    ini_set "$file" keystone_authtoken auth_url $keystone_unversioned_admin_url
+    ini_set "$file" keystone_authtoken auth_type password
+    ini_set "$file" keystone_authtoken project_name $trove_project
+    ini_set "$file" keystone_authtoken username $trove_user
+    ini_set "$file" keystone_authtoken password $trove_password
+    [ -n "${trove_region}" ] && ini_set "$file" keystone_authtoken region_name $trove_region
+done
 
 # Get information out of keystone and put in trove conf files.
 function keystone_cmd {
@@ -182,9 +169,6 @@ function keystone_cmd {
 }
 export -f keystone_cmd
 
-# Get the id for the admin tenant from keystone and add to trove-taskmanager.conf
-nova_proxy_admin_tenant_id=$( keystone_cmd tenant-get $nova_tenant | awk '/\ id\ / {print $4}' )
-ini_set /etc/trove/trove-taskmanager.conf DEFAULT nova_proxy_admin_tenant_id "${nova_proxy_admin_tenant_id}"
-ini_set /etc/trove/trove.conf DEFAULT nova_proxy_admin_tenant_id "${nova_proxy_admin_tenant_id}"
-
-chown trove:trove /var/log/trove/trove-api.log
+if [ -f /var/log/trove/trove-api.log ]; then
+    chown trove:trove /var/log/trove/trove-api.log
+fi
