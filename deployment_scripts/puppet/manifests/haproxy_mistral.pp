aa5b554408c6dc9d1a2f606b4118af6ddbf51026
iduodu <iduodu@tesora.com>
2016-10-10 11:52:20 -0400

    Fix mistral_haproxy script && delete unused scripts
    Change-Id: I4fd09270b87befd8251dc151d596bd8c23a81b5b

--- deployment_scripts/puppet/manifests/haproxy_mistral.pp
+++ deployment_scripts/puppet/manifests/haproxy_mistral.pp
@@ -7,3 +7,30 @@
 
 notice('tesora_mistral haproxy_mistral.pp')
 
+class tesora_mistral::haproxy_mistral {
+
+  openstack::ha::haproxy_service { 'mistral-api':
+    internal_virtual_ip    => $tesora_mistral::management_vip,
+    listen_port            => $tesora_mistral::port,
+    order                  => '300',
+    public_virtual_ip      => $tesora_mistral::public_vip,
+    internal               => true,
+    public                 => true,
+    ipaddresses            => $tesora_mistral::mistral_api_nodes_ips,
+    server_names           => $tesora_mistral::mistral_api_nodes_ips,
+    public_ssl             => $tesora_mistral::public_ssl,
+    public_ssl_path        => $tesora_mistral::public_ssl_path,
+    haproxy_config_options => {
+        option         => ['httpchk', 'httplog', 'httpclose'],
+        'http-request' => 'set-header X-Forwarded-Proto https if { ssl_fc }',
+    },
+    balancermember_options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3',
+  }
+
+  firewall { '300 mistral':
+    chain  => 'INPUT',
+    dport  => $tesora_mistral::port,
+    proto  => 'tcp',
+    action => 'accept',
+  }
+}
