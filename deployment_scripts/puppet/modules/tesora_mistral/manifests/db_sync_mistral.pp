3cdfcbe218f59afecf37293a1f171c120e05b9db
Brian Hunter <bhunter@tesora.com>
2016-10-12 14:51:03 -0400

    Revert "remove db-sync upgrade for mistral"
    This reverts commit d77c349c1b260f41d7df47c6d78c5dd367bb6850.
    
    Change-Id: I58486efc82846764e78ef05a1522ec4267c38c93

--- deployment_scripts/puppet/modules/tesora_mistral/manifests/db_sync_mistral.pp
+++ deployment_scripts/puppet/modules/tesora_mistral/manifests/db_sync_mistral.pp
@@ -22,6 +22,15 @@ class tesora_mistral::db_sync_mistral (
     content => template('tesora_mistral/mistral_dbsync.conf.erb')
   }
 
+  exec { 'tesora_mistral-dbsync-upgrade':
+    command     => 'mistral-db-manage --config-file=/etc/mistral/mistral_dbsync.conf upgrade 011',
+    path        => '/usr/bin',
+    user        => 'root',
+    refreshonly => true,
+    logoutput   => on_failure,
+    subscribe   => File['/etc/mistral/mistral_dbsync.conf'],
+  }
+
   exec { 'tesora_mistral-dbsync-populate':
     command     => 'mistral-db-manage --config-file=/etc/mistral/mistral_dbsync.conf populate',
     path        => '/usr/bin',
@@ -32,5 +41,6 @@ class tesora_mistral::db_sync_mistral (
   }
 
   File['/etc/mistral/mistral_dbsync.conf'] ->
-    Exec['tesora_mistral-dbsync-populate']
+    Exec['tesora_mistral-dbsync-upgrade'] ->
+      Exec['tesora_mistral-dbsync-populate']
 }
