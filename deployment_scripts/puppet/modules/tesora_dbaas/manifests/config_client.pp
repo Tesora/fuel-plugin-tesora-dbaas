04446bff5d9e418f48c251d15ede002319be1105
Brian Hunter <bhunter@tesora.com>
2016-10-19 10:58:43 -0400

    make tesora openrc more consistent with Fuel/keystone environment.
    Generate a /root/openrc on the dbaas node like that on the
    controller, used to access the keystone admin account.
    Generate a trove openrc that is identical, but contains additional
    variables.
    
    Change-Id: Ib82d1ad003a441cafe6d6a65972efdd71e4f0a03

--- deployment_scripts/puppet/modules/tesora_dbaas/manifests/config_client.pp
+++ deployment_scripts/puppet/modules/tesora_dbaas/manifests/config_client.pp
@@ -5,20 +5,44 @@
 # except as may be expressly permitted in the applicable license agreement.
 #
 
+# This creates two files of importance
+# we create the /root/openrc to look like it does on the controller.
+# We then create our tesora openrc file that contains those contents, plus
+# additional Tesora DBaaS related environment variables.
+
 class tesora_dbaas::config_client (
     $os_auth_url = undef,
     $os_user = undef,
     $os_pass = undef,
     $os_tenant_name = undef,
+    $os_region = undef,
     $guest_download_user = undef,
     $guest_download_pass = undef,
     $dbaas_release = undef,
     $dbaas_version = undef,
     $dbaas_repo = undef,
 ) {
-  file { '/opt/tesora/dbaas/bin/openrc.sh':
-    ensure  => file,
-    content => template('tesora_dbaas/openrc.sh.erb')
+  require osnailyfacter::credentials_file
+
+  osnailyfacter::credentials_file { '/root/openrc':
+    admin_user          => $os_user,
+    admin_password      => $os_pass,
+    admin_tenant        => $os_tenant_name,
+    region_name         => $os_region,
+    auth_url            => $os_auth_url,
+  }
+
+  concat { '/opt/tesora/dbaas/bin/openrc.sh': }
+
+  concat::fragment { 'standard-openrc-admin-contents':
+    target   => '/opt/tesora/dbaas/bin/openrc.sh',
+    order    => '01',
+    contents => file('/root/openrc'),
+  }
+
+  concat::fragment { 'tesora-additional-vars':
+    target   => '/opt/tesora/dbaas/bin/openrc.sh',
+    order    => '02',
+    contents => template('tesora_dbaas/tesoradbaasrc.sh.erb'),
   }
 }
-
