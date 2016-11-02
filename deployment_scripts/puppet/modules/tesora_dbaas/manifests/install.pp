546ac81fcadc988a8e17f13648afe8a2415c6077
Brian Hunter <bhunter@tesora.com>
2016-11-02 16:52:50 -0400

    improved ordering around db_sync and service starts
    install packages but stop services
    ...
    db_sync
    configure, which starts/enables services
    
    Change-Id: If5d0db192fbf85bbca238a77ee77ef7f04b0fe86

--- deployment_scripts/puppet/modules/tesora_dbaas/manifests/install.pp
+++ deployment_scripts/puppet/modules/tesora_dbaas/manifests/install.pp
@@ -10,4 +10,18 @@ class tesora_dbaas::install {
   package { 'tesora-dbaas-taskmanager': ensure => 'installed' }
   package { 'tesora-dbaas-conductor':   ensure => 'installed' }
   package { 'tesora-dbaas-client':      ensure => 'installed' }
+
+  service { 'trove-api':
+    ensure  => stopped,
+    require => Package['tesora-dbaas-api']
+  }
+  service { 'trove-taskmanager':
+    ensure  => stopped,
+    require => Package['tesora-dbaas-taskmanager']
+  }
+  service { 'trove-conductor':
+    ensure  => stopped,
+    require => Package['tesora-dbaas-conductor']
+  }
+
 }
